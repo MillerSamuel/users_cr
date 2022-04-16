149de65ac4d6ee3791719b8ed0d3cd5c6b810261
Samuel Miller <smillercb@gmail.com>
2022-04-16 02:22:23 -0400

    commit

--- server.py
+++ server.py
@@ -3,15 +3,19 @@ from user import User
 app=Flask(__name__)
 
 @app.route("/")
+def start():
+    return redirect("/users")
+
+@app.route("/users")
 def usersindex():
     users=User.get_all()
     return render_template("read.html",users=users)
 
-@app.route("/new")
+@app.route("/users/new")
 def newindex():
     return render_template("create.html")
 
-@app.route("/createnew", methods=["post"])
+@app.route("/users/createnew", methods=["post"])
 def createnew():
     data={
         "first_name":request.form["first_name"],
@@ -20,7 +24,36 @@ def createnew():
     }
     print(data)
     User.save(data)
-    return redirect("/")
+    return redirect("/users")
+
+@app.route("/users/edit/<int:id>")
+def edit(id):
+    data={
+        "id":id
+    }
+    return render_template ("edit.html",user=User.get_one(data))
+
+@app.route("/users/show/<int:id>")
+def show(id):
+    data={
+        "id":id
+    }
+    return render_template("readone.html",user=User.get_one(data))
+
+@app.route("/users/update",methods=["post"])
+def update():
+    User.update(request.form)
+    return redirect("/users")
+
+@app.route("/users/delete/<int:id>")
+def delete(id):
+    data={
+        "id":id
+    }
+    print("deleting maybe")
+    User.delete(data)
+    return redirect("/users")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
